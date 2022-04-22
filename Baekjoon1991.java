5154dd7477ceee914e10d37974370f372eb4fac1
ljm97 <58106556+dksk678@users.noreply.github.com>
2022-04-22 19:59:44 +0900

    Create Baekjoon1991.java
    트리 순회
    트리, 재귀

---
+++ Baekjoon1991.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+/*  트리 순회  1991  90
+ * 
+ *  Node 생성
+ *  -> 3개의 순회 방법 구하기
+ *  
+ *  pre: par->left->right
+ *  in: left->par->right
+ *  post: left->right->par
+ */
+
+class Baekjoon1991 {
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+//	static int N;
+//	static int M;
+	static int T;
+
+	static String preans="";
+	static String inans="";
+	static String postans="";
+	static HashMap<String, Node> tree;
+	
+	static class Node{
+		String left;
+		String right;
+		
+		public Node(String left, String right) {
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        st = new StringTokenizer(bf.readLine());
+        T = Integer.parseInt(bf.readLine());
+
+        tree = new HashMap<String, Node>();
+
+        for(int i=0; i<T; i++) {
+        	st = new StringTokenizer(bf.readLine());
+
+        	tree.put(st.nextToken(), new Node(st.nextToken(), st.nextToken()));
+        }
+        
+        order("A");
+
+        sb.append(preans+"\n").append(inans+"\n").append(postans+"\n");
+        System.out.println(sb);
+	}
+
+	static void order(String cur) {
+		if (cur.equals(".")) return;
+		
+		preans += cur; 
+		order(tree.get(cur).left);
+		inans += cur;
+		order(tree.get(cur).right);
+		postans += cur;
+	}
+}
