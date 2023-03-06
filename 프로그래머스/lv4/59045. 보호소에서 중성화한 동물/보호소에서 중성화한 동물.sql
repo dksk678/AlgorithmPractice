dc30b0f188c2da040e6bfcae612dba3224a32057
ljm0726 <58106556+ljm0726@users.noreply.github.com>
2023-03-06 19:20:22 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,19 @@
+-- 코드를 입력하세요
+# 나가기 전에는 중성화 X 나간 후 중성화 된 동물.
+# OUTS에서 중성화 된 동물 뽑기, 그 동물들 중에서 upon이 다른 동물들 출력
+
+# SELECT *
+# FROM ANIMAL_INS
+
+# SELECT ANIMAL_ID, SEX_UPON_OUTCOME
+#     FROM ANIMAL_OUTS
+#     WHERE SEX_UPON_OUTCOME = 'Spayed Female' OR SEX_UPON_OUTCOME = 'Neutered Male'
+
+SELECT I.ANIMAL_ID, I.ANIMAL_TYPE, I.NAME
+FROM ANIMAL_INS I JOIN (
+    SELECT ANIMAL_ID, SEX_UPON_OUTCOME
+    FROM ANIMAL_OUTS
+    WHERE SEX_UPON_OUTCOME = 'Spayed Female' OR SEX_UPON_OUTCOME = 'Neutered Male'
+) AS O
+ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.SEX_UPON_INTAKE != O.SEX_UPON_OUTCOME
