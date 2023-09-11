09c7e1a59b8b314479204888c711561ccf0f26f9
ljm0726 <58106556+ljm0726@users.noreply.github.com>
2023-09-11 19:48:44 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 57.15 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667#) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 49.31 ms
+메모리: 97.8 MB, 시간: 57.15 ms
 
 ### 구분
 
--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 /*
     원소의합을 같게 만들어야 함 => 각 배열의 합을 더한후 / 2
@@ -7,33 +6,29 @@ import java.util.stream.Collectors;
         
     무조건 pop을 사용해서 숫자 교환을 해야함.
     최소의 pop을 사용해서 합이 같은 경우를 찾아야함.
-    -> 완탐, 부분집합
+    -> 완탐
 */
 class Solution {
     private static int answer = -1;  // min Count
     
     public int solution(int[] queue1, int[] queue2) {
-        Queue<Double> q1 = new ArrayDeque<>();
-        Queue<Double> q2 = new ArrayDeque<>();
+        Queue<Integer> q1 = new ArrayDeque<>();
+        Queue<Integer> q2 = new ArrayDeque<>();
         
-        Double q1Sum = 0d;
-        Double q2Sum = 0d;
+        Long q1Sum = 0L;
+        Long q2Sum = 0L;
         
         for(int i=0,len=queue1.length; i<len; i++){
-            q1.offer((double)queue1[i]);
-            q2.offer((double)queue2[i]);
+            q1.offer(queue1[i]);
+            q2.offer(queue2[i]);
             
-            q1Sum += (double)queue1[i];
-            q2Sum += (double)queue2[i];
+            q1Sum += queue1[i];
+            q2Sum += queue2[i];
         }
-        
-        // Long q1Sum = q1.stream().reduce(0L, (a, b) -> a + b);
-        // Long q2Sum = q2.stream().reduce(0L, (a, b) -> a + b);
-
         
         if((q1Sum + q2Sum) % 2 == 1) return answer; //합이 홀수인 경우 각 큐 합을 동일하게 만들 수 없음.
         
-        findAnswer(q1Sum, q2Sum, q1, q2); //sum, Queue
+        findAnswer(q1Sum, q2Sum, q1, q2); //sum12, Queue
         
         return answer;
     }
@@ -41,20 +36,19 @@ class Solution {
     /**
     * 최소 횟수인 answer 구하기
     */
-    public void findAnswer(double q1Sum, double q2Sum, Queue<Double> q1, Queue<Double> q2) {
+    public void findAnswer(long q1Sum, long q2Sum, Queue<Integer> q1, Queue<Integer> q2) {
         int qSize = q1.size();
-        int end = qSize * 4;
+        int end = (qSize * 2) * 2;
         int popCount = 0;
         
         while(q1Sum != q2Sum) {
-            
             if(q1Sum > q2Sum)  {
-                Double peek = q1.poll();
+                int peek = q1.poll();
                 q2.offer(peek);
                 q1Sum -= peek;
                 q2Sum += peek;
             } else if (q1Sum < q2Sum){
-                Double peek = q2.poll();
+                int peek = q2.poll();
                 q1.offer(peek);
                 q1Sum += peek;
                 q2Sum -= peek;
