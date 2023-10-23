36012adf12d41510e055217fd0fbd675646cc3ee
ljm0726 <58106556+ljm0726@users.noreply.github.com>
2023-10-24 00:40:53 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.84 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 0.50 ms
+메모리: 73.3 MB, 시간: 0.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 0:36:49
+2023년 10월 2일 0:40:51
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,60 +1,47 @@
 import java.util.*;
-
-/*
-최대 n*2 미만임. 1~500 총합 즉 12만 -> remove를 사용하면 *2 25만?
-1. 끝나는 시간이 가장 빠른 순으로 정렬
-2. 시작시간이 가장 빠른 순으로 정렬
-
-끝나는 시간이 가장 빠른 순으로 찾다가 시작 시간이 현재 시간이 되는 경우가 되면 다음 으로 넘김
-
-*/
 class Solution {
-    
-    public int solution(int[][] jobs) {
-        int jlen = jobs.length;
-        //1. 나는 시간이 가장 빠르게, 2. 같을 경우 시작시간이 가장 빠르게 
-        Arrays.sort(jobs, ((o1, o2) -> {
-            int ediff = o1[1] - o2[1];
-            if(ediff == 0) {
-                return o1[0] - o2[0];
-            }
-            return ediff;
-        }));
-        
-        ArrayList<int[]> jobList = new ArrayList<>();
-        int curTime = 0; //처음 시작하는 시간 저장
-        
-        for(int i=0; i<jlen; i++ ) {
-            curTime = jobs[i][0] < curTime ? jobs[i][0] : curTime;
-            jobList.add(new int[] {jobs[i][0], jobs[i][1]});
-        }
-        
-        int totTime = 0;
-        while(!jobList.isEmpty()) {
-            int start = 1001;
-            int snum = 0;
-            
-            for(int i=0; i<jobList.size(); i++) {
-                if(curTime >= jobList.get(i)[0]) { //종료시간이 가장 빠른 것 중에 이미 시작시간인 경우 바로 break;
-                    snum = i;
-                    break;
-                } else if(jobList.get(i)[0] < start){ //아니면 가장 빠른 시작시간 찾기
-                    start = jobList.get(i)[0];
-                    snum = i;
-                }
-            }
-            
-            int[] curJob = jobList.get(snum);
-            if(curTime < curJob[0]) { //가장 빠른 시작시간이 현재시간보다 긴 경우
-                curTime = curJob[0]; //현재시간을 가장 빠른시작시간으로 변경
-            }
-            
-            curTime += curJob[1];
-            totTime += curTime - curJob[0];
-            
-            jobList.remove(snum);
-        }
-        
-        return totTime/jlen;
+public static int solution(int[][] jobs) {
+
+      Arrays.sort(jobs, new Comparator<int[]>() {
+          public int compare(int[] o1, int[] o2) {
+              if(o1[0] <= o2[0]){
+                  return -1;
+              }
+              return 1;
+          }
+      });      
+
+      PriorityQueue<int[]> queue = new PriorityQueue<int[]>(new Comparator<int[]>() {
+          public int compare(int[] o1, int[] o2) {
+              if(o1[1] < o2[1]){
+                  return -1;
+              }
+              return 1;
+          }
+      });
+
+      int time = 0;
+      int index = 0;
+      float answer = 0;
+
+      while(true){
+          while(index < jobs.length && jobs[index][0] <= time){
+              queue.offer(jobs[index]);
+              index ++;
+          }
+          if(queue.size() == 0){
+              time = jobs[index][0];
+              continue;
+          }
+          int[] job = queue.poll();
+          time += job[1];
+          answer += time - job[0];
+          if(index == jobs.length && queue.size() == 0){
+              break;
+          }
+      }
+
+      answer /= jobs.length;
+      return (int)answer;
     }
 }
